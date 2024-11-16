c9f895275428c35f98c2904b0ea4856104dc368e
Rafael Miralha <rafamiralha1@gmail.com>
2024-11-15 21:56:45 -0300

    feat: criação base dos projetos

---
+++ second-design-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+        teste 2
+    </div>
+  );
+}
---
+++ third-design-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+        teste 3
+    </div>
+  );
+}
